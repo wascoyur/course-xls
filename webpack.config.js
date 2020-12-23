24b761f0d179d3915f16524608a551478550562a
mku <mytem20007@gmail.com>
2020-12-23 14:47:44 +0500

    begin 17

--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,91 @@
 const path = require('path');
-let  HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isProd = process.env.NODE_ENV ==='production';
 const isDev = !isProd;
+const isLoaders = () =>{
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+      },
+    },
+  ]
+  if (isDev) {
+    loaders.push('eslint-loader')
+  }
+  return loaders
+}
 
-console.log('is prod:',isProd);
-console.log('is dev:',isDev);
+console.log('is prod:', isProd);
+console.log('is dev:', isDev);
 
 module.exports ={
-    context: path.resolve(__dirname, 'src'),
-    mode: "development",
-    entry: ['@babel/polyfill','./index.js'],
-    output: {
-        filename: "bundle.[hash:4].js",
-        path: path.resolve(__dirname, 'dist')
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: ['@babel/polyfill', './index.js'],
+  output: {
+    filename: 'bundle.[hash:4].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve((__dirname, 'src/core')),
     },
-    resolve: {
-        extensions: ['.js'],
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-            '@core': path.resolve((__dirname, 'src/core'))
-        }
-    },
-    devtool: isDev ? 'source-map': false,
-    devServer: {
-        port:3000,
-        hot:isDev
-    },
-    plugins:[
-        new HtmlWebpackPlugin({
-            template: "index.html",
-            minify: {
-                removeComments: isProd,
-                collapseWhitespace: isProd
-            }
-        }),
-        new CleanWebpackPlugin(),
-        new CopyPlugin({
-            patterns:[
-                {
-                    from: path.resolve(__dirname, 'src/favicon.ico'),
-                    to: path.resolve(__dirname, 'dist')
-                }
-            ]}),
-        new MiniCssExtractPlugin({
-            filename: 'bundle.[hash:4].css'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            hmr:isDev,
-                            reloadAll: true
-                        }
-                    },
-                    "css-loader",
-                    "sass-loader",
-                ],
+  },
+  devtool: isDev ? 'source-map': false,
+  devServer: {
+    port: 3000,
+    hot: isDev,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ]}),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.[hash:4].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true,
             },
-            {
-                test:/\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: ['@babel/plugin-proposal-object-rest-spread']
-                    }
-                }
-            }
+          },
+          'css-loader',
+          'sass-loader',
         ],
-    },
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: isLoaders(),
+      },
+    ],
+  },
 
 }
